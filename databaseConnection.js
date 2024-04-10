10a91dfc77a4ce8e7c53019d0663b7074727d484
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-04-09 18:03:35 -0700

    trying to fix connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,11 +1,11 @@
-const database = require("mongoose");
-const is_render = process.env.IS_RENDER || false;
-const databaseName = "lab_example"
+const mongoose = require("mongoose");
+const isRender = process.env.IS_RENDER === 'true';
+const databaseName = "lab_example";
 const renderURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.t1y1wqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const localURI = "mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true"
-if (is_render) {
-database.connect(renderURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-else {
-database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
+const localURI = `mongodb://localhost:27017/${databaseName}?authSource=admin&retryWrites=true`;
+
+const connectURI = isRender ? renderURI : localURI;
+
+mongoose.connect(connectURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.error("Database connection error:", err));
